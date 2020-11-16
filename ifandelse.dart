0b6272699a42baab3f0adb7b22671d41068e254c
JunSung Go <gojs712@gmail.com>
2020-11-16 19:25:34 +0900

    Create ifandelse.dart

---
+++ ifandelse.dart
@@ -0,0 +1,22 @@
+void main() {
+  
+  var salary = 20000;
+  
+  if (salary > 20000) {
+    print("You got promotion. Congratulations!");
+  } else {
+    print("You need to work hard!");
+  }
+  
+  var marks = 99;
+  
+  if (marks >= 90 && marks < 100) {
+    print("A grade!");
+  } else if (marks >= 80 && marks < 90) {
+    print("B grade");
+  } else if (marks >= 70 && marks < 80) {
+    print("C grade");
+  } else if (marks >= 60 && marks < 70) {
+    print("D grade");
+  }
+}
